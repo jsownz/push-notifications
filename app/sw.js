b8172512265cd978ea9da309629c1618cee35daa
Jason Sohl <jason@frozensoliddesigns.com>
2016-04-27 15:15:33 -0700

    push notification testing

--- app/sw.js
+++ app/sw.js
@@ -19,6 +19,55 @@
 
 // Version 0.1
 
+// GCM API Key
+// AIzaSyAWgCdIi8VP9qqN8gfNIt41GK-WhRC4Xgo
+// Project Number
+// 546762979088
+// https://android.googleapis.com/gcm/send/cB20XudAfzg:APA91bGk35ijTJtzZGTrKf9w4d4l4fcnhtUOoNbcht9xW3UfpN_vAFqepi1xM5hGjglOpAfRIDv4cjg768ZPI1fgJR8hUFaUexGmaVsHyBKkV-IeS7d1DZ4SxNDsM2G0pQiHEfkKRyGy
+// SubscriptionId
+// cB20XudAfzg:APA91bGk35ijTJtzZGTrKf9w4d4l4fcnhtUOoNbcht9xW3UfpN_vAFqepi1xM5hGjglOpAfRIDv4cjg768ZPI1fgJR8hUFaUexGmaVsHyBKkV-IeS7d1DZ4SxNDsM2G0pQiHEfkKRyGy
+// curl command
+// curl --header "Authorization: key=AIzaSyAWgCdIi8VP9qqN8gfNIt41GK-WhRC4Xgo" --header "Content-Type: application/json" https://android.googleapis.com/gcm/send -d "{\"registration_ids\":[\"cB20XudAfzg:APA91bGk35ijTJtzZGTrKf9w4d4l4fcnhtUOoNbcht9xW3UfpN_vAFqepi1xM5hGjglOpAfRIDv4cjg768ZPI1fgJR8hUFaUexGmaVsHyBKkV-IeS7d1DZ4SxNDsM2G0pQiHEfkKRyGy\"]}"
+
+
 'use strict';
 
-// TODO
+console.log('started ', self);
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+  console.log('Installed', event);
+});
+self.addEventListener('activate', function(event) {
+  console.log('Activated', event);
+});
+self.addEventListener('push', function(event) {
+  console.log('Push message received', event);
+  var title = 'Push Message';
+  event.waitUntil(
+    self.registration.showNotification(title, {
+      body: 'The Message',
+      icon: 'images/icon.png',
+      tag: 'my-tag'
+    }));
+});
+self.addEventListener('notificationclick', function(event) {
+  console.log('Notification Click: tag ', event.notification.tag);
+  event.notification.close();
+  var url = 'https://www.google.com';
+  event.waitUntil(
+    clients.matchAll({
+      type: 'window'
+    })
+    .then(function(windowClients) {
+      for (var i = 0; i < windowClients.length; i++) {
+        var client = windowClients[i];
+        if (client.url === url && 'focus' in client) {
+          return client.focus();
+        }
+      }
+      if (clients.openWindow) {
+        return clients.openWindow(url);
+      }
+    })
+  );
+});
